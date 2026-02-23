849054ebc8e83aef13bb6fa5cc528bfc0cb41adf
joaquinr765-commits <joaquinr765@gmail.com>
2026-02-24 00:37:40 +0100

    agrego carpeta javascript

---
+++ Javascript/personalCard.js
@@ -0,0 +1,48 @@
+// HOMEWORK VARIABLES
+const fullName = "Joaquin Rodriguez";
+let profileAge = 27;
+let country = "Germany";
+const favoriteHobby = "Airsoft";
+let isStudent = true;
+
+console.log(`
+    ---- PROFILE CARD ----
+    Name: ${fullName} 
+    Age: ${profileAge} 
+    Country: ${country} 
+    Hobby: ${favoriteHobby} 
+    Is Student? ${isStudent}
+    `);
+
+// HOMEWORK DATA TYPES
+
+let dayName = "Monday";
+let stepsWalked = 8423;
+let waterCups = 8;
+let didWorkout = false;
+let eveningPlan = null;
+let favoriteSnack = undefined;
+
+console.log(`
+    ---- DAILY STATS ----
+    Day: ${dayName}
+    Steps walked: ${stepsWalked}
+    Water cups: ${waterCups}
+    Worked out today? ${didWorkout}
+    Evening plan: ${eveningPlan}
+    Favorite snack: ${favoriteSnack}
+     `);
+
+eveningPlan = "Go to the Gym";
+favoriteSnack = "Pringles";
+
+console.log(`
+    ---- DAILY STATS ----
+    Day: ${dayName}
+    Steps walked: ${stepsWalked}
+    Water cups: ${waterCups}
+    Worked out today? ${didWorkout}
+    Evening plan: ${eveningPlan}
+    Favorite snack: ${favoriteSnack}
+    `);
+
