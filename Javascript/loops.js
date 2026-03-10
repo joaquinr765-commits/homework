0420cc5ebaa7b221682d0e9dd2cd7d868e32c0e7
joaquinr765-commits <joaquinr765@gmail.com>
2026-03-10 18:28:56 +0100

    subo javascript

---
+++ Javascript/loops.js
@@ -0,0 +1,81 @@
+// ----HOMEWORK----
+
+let baseNumber = 5;
+console.log("--- MULTIPLICATION TABLE ---");
+for (let i = 1; i <= 10; i++) {
+    let result = baseNumber * i;
+    console.log(`${baseNumber} x ${i} = ${result}`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Counting up; ${i}`)
+// }
+
+// let maxNumber = 5;
+// let total = 0;
+
+// for (let total = 1; total <= maxNumber; total++) {
+//     console.log(`Counting up: ${total}`)
+// }
+
+// for (let i = 1; i<= maxNumber; i++) {
+//     total += i
+// }
+// console.log(`Sum of numbers from 1 to ${maxNumber} is ${total}`)
+
+
+// for (let i = 5; i >= 1; i--) {
+//     console.log(`Counting down ${i}`);
+// }
+
+
+// const favSnack = ["Chips", "Chocolate", "Mango", "Ice Cream"];
+
+// for (let i = 0; i < favSnack.length; i++ ) {
+//     console.log(`Snacks ${i}: ${favSnack[i]}`)
+// }
+
+// let number = 1;
+// while(number <= 5) {
+//     console.log(`While counting up: ${number}`);
+//     number += 1;
+// }
+
+
+// let countdown = 5;
+// while(countdown > 0) {
+//     console.log(`While countdown: ${countdown}`);
+//     countdown -= 1;
+// }
+
+// let limit = 5;
+// let current = 1;
+// let sum = 0;
+
+// while(current <= limit) {
+//     sum = sum + current
+//     current += 1 //current ++ also do the same and looks cleaner
+//     console.log(`While loop sum from 1 to ${limit} is ${sum} `)
+// }
+
+// let dailyTasks = ["Work", "Gym", "Cook"];
+// let index = 0;
+
+// while(index < dailyTasks.length) {
+//     console.log(`Task ${index}: ${dailyTasks[index]}`);
+//     index++;
+// }
